a49a11018f727447de7b3cf154758164491b84d8
MirrorN <iswuxing@163.com>
2022-11-18 12:37:24 +0800

    vite

---
+++ Vite/vue3-basics/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Test from './components/views/Test.vue'
+</script>
+
+<template>
+  <div>
+    <Test></Test>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ Vite/vue3-basics/src/components/views/Test.vue
@@ -0,0 +1,35 @@
+<!-- Test component -->
+<template>
+  <div>
+    <h3>this is a test sentence</h3>
+    {{ count }}
+    <!-- <button v-on="click()">add</button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  // mounted: {},
+
+  methods: {
+    click() {
+      this.count += 1
+    },
+  },
+}
+</script>
+<style lang="css" scoped>
+h3 {
+  color: red;
+}
+</style>
