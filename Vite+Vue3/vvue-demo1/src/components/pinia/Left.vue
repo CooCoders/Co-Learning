1859704bb1e4f08fe7bd1cd08849d89f47d2c7b4
MirrorN <iswuxing@163.com>
2022-12-14 20:43:03 +0800

    pinia

--- Vite+Vue3/vvue-demo1/src/components/pinia/Left.vue
+++ Vite+Vue3/vvue-demo1/src/components/pinia/Left.vue
@@ -2,13 +2,16 @@
 <template>
   <div class="left-container">
     <h3>Left component</h3>
-    <p>Count: {{ useCounterStore.count }}</p>
+    <p>Count: {{ counterStore.count }}</p>
+    <button @click="counterStore.add()">add</button>
+    <button @click="counterStore.sub()">sub</button>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue'
 import useCounterStore from '@/store/counterStore.js'
+const counterStore = useCounterStore()
 </script>
 <style scoped>
 .left-container {
